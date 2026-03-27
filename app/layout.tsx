5aceba824034b0fc484e262238b10f7125c1aa45
Joel Miller <millerjoel7597@gmail.com>
2026-03-27 14:36:58 +0100

    refactor: use environment variable for GA ID instead of hardcoding

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,21 +83,25 @@ export default function RootLayout({
         </BookStoreProvider>
         <Toaster position="bottom-right" />
         {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-R4T4TFHY1Q"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-R4T4TFHY1Q', {
-              page_path: window.location.pathname,
-            });
-            console.log("ITABC Analytics Initialized");
-          `}
-        </Script>
+        {process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+                console.log("ITABC Analytics Initialized");
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   );
